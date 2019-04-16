a402e65da5a72c00492d074fd8a16d9e1c7bd6b1
Nataly <kumpannatali@yandex.ru>
2019-04-16 20:12:26 +0300

    Lesson 4 (normal)

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,6 +20,22 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# 1 вариант
+import re
+mod=re.findall("[a-z]{1,}",line)
+print(mod)
+# 2 вариант 
+lst = []
+a = ''
+for symbol in range(len(line)):
+    if line[symbol].islower():
+        a += line[symbol]
+    else:
+        if line[symbol - 1].islower():
+            lst.append(a)
+            a = ''
+print(lst)
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +61,41 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+	   
+# 1 вариант
+first=re.findall("[a-z]{2}([A-Z]{1,})[A-Z]{2}",line_2)
+print(first)
+# 2 вариант
+
+lst = []
+a = ''
+for symbol in range(len(line_2)):
+    if line_2[symbol].isupper():
+        a += line_2[symbol]
+    else:
+        if line_2[symbol - 1].isupper():
+            lst.append(a)
+            a = ''
+print(lst)
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+import re
+
+with open(r'C:\Users\Наташа\Desktop\re.txt', 'w') as my_file:
+    for i in range(2500):
+        my_file.write(str(random.randint(0, 9)))
+with open(r'C:\Users\Наташа\Desktop\re.txt', 'r') as my_file:
+    longest = list()
+    huge = my_file.read()
+    for number in range(0, 10):
+        if str(number) in huge:
+            lst = re.findall('[%s]+' % number, huge)
+            longest.append(max(lst, key=len))
+    result = max(longest, key=len)
+    print(result)
