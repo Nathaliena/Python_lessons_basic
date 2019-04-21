f9d4397b802023fa87ac8495da855607eeae669e
Nataly <kumpannatali@yandex.ru>
2019-04-21 20:39:40 +0300

    Lesson 5. Выполнила задания easy

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,38 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+
+try:
+    for i in range(1,3):
+        os.mkdir("dir_" + str(i))
+except:
+    print("Done")
+
+try:
+    for i in range(1,3):
+        os.rmdir("dir_" + str(i))
+except:
+    print("Removed")
+
+
+
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+import sys
+
+print("sys.path= ",sys.path)
+
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+import shutil
+
+shutil.copy('гик_5.py','гик_5_copy.py')
+
+
